2bed74c6f24561eec9913c17f9f615db0716c173
GitKKg <lancexk@gmail.com>
2019-11-15 13:27:30 +0800

    Chapter ExistentialTypes

---
+++ ExistentialTypes.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+--{-# LANGUAGE DatatypeContexts #-}
+{-# LANGUAGE RankNTypes #-}
+
+{-# LANGUAGE GADTs #-}
+
+{-# LANGUAGE ConstraintKinds #-}
+
+-- {-# LANGUAGE ExistentialQuantification #-}
+-- | 
+
+module Haskell.ThinkingWithTypes.ExistentialTypes where
+
+
+import Data.Typeable
+import Data.Maybe
+import Data.Foldable
+
+import Data.Kind (Constraint, Type)
+-- data Any = forall a. Any a
+
+data Any where
+  Any :: a -> Any
+
+elimAny :: (forall a. a -> r) -> Any -> r
+elimAny f (Any a) = f a
+
+func :: forall a . forall r. (a -> r) -> a -> r
+func ar a = ar a
+
+contPlus a b c d = a + b + c + d
+
+sum = contPlus 1 `func` 2 `func` 3 `func` 4
+
+data HasShow where
+  HasShow :: Show t =>  t -> HasShow
+
+elimHasShow :: (forall a. Show a => a -> r)  -> HasShow  -> r
+elimHasShow f (HasShow a) = f a
+
+-- geta ::  HasShow ->  (forall a. Show a => a)
+-- geta (HasShow a) = a
+
+instance Show HasShow where
+  show hs = elimHasShow show hs
+
+
+data Dynamic where
+  Dynamic :: Typeable t => t -> Dynamic
+
+elimDynamic :: (forall a. Typeable a => a -> r)
+  -> Dynamic
+  -> r
+elimDynamic f (Dynamic a) = f a
+
+fromDynamic :: Typeable a => Dynamic -> Maybe a
+fromDynamic = elimDynamic cast
+
+liftD2 :: forall a b r.
+  ( Typeable a
+  , Typeable b
+  , Typeable r
+  )
+  => Dynamic
+  -> Dynamic
+  -> (a -> b -> r)
+  -> Maybe Dynamic
+
+liftD2 d1 d2 f =
+  fmap Dynamic . f -- (Dynamic .) . f also works
+  <$> fromDynamic @a d1
+  <*> fromDynamic @b d2
+
+pyPlus :: Dynamic -> Dynamic -> Dynamic
+pyPlus a b =
+  fromMaybe (error "bad types for pyPlus") $ asum
+  [ liftD2 @String @String a b (++)
+  , liftD2 @Int @Int a b (+)
+  , liftD2 @String @Int a b $ \strA intB ->
+      strA ++ show intB
+  , liftD2 @Int @String a b $ \intA strB ->
+      show intA ++ strB
+  ]
+
+pyi = fromDynamic @Int (pyPlus ( Dynamic (1 :: Int)) ( Dynamic (2 ::Int)))
+
+
+data Has (c :: Type -> Constraint) where
+  Has :: c t => t -> Has c  -- here c is Dynamic or Show, Monoid, Monad ...
+
+elimHas
+  :: (forall a. c a => a -> r)
+  -> Has c
+  -> r
+
+elimHas f (Has a) = f a
