c4b46b1d676a593fa5f8ff64e0cf1d21014dd35e
GitKKg <lancexk@gmail.com>
2020-01-22 16:36:39 +0800

    Chapter 13.3 UsingGenericMetadata

---
+++ UsingGenericMetadata.hs
@@ -0,0 +1,43 @@
+-- | 
+
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Haskell.ThinkingWithTypes.UsingGenericMetadata where
+
+import Control.Monad.Writer
+import Data.Aeson (Value (..), (.=), object)
+import Data.Kind (Type)
+import Data.Text (Text, pack)
+import Data.Typeable
+import Data.Vector (fromList)
+import GHC.Generics
+import GHC.TypeLits
+import qualified GHC.TypeLits as Err
+
+data Person = Person
+  { name :: String
+  , age  :: Int
+  , phone :: Maybe String
+  , permissions :: [Bool]
+  }
+  deriving (Generic)
+
+class GSchema (a :: Type -> Type) where
+  gschema :: Writer [Text] Value
+
+mergeObjects :: Value -> Value -> Value
+mergeObjects (Object a) (Object b) = Object $ a <> b
+
+emitRequired :: forall nm. KnownSymbol nm => Writer [Text] ()
+emitRequired = tell . pure . pack . symbolVal $ Proxy @nm
